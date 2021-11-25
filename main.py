c7698209f666c58b3887e30ed43d7bbf4998fec2
pompyn <pompyn@live.com>
2021-11-25 15:42:03 -0600

    add ability to remove from list

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ while True:
         remove = input("Would you like to be removed from the system? (y/n) ".lower())
         if remove == "y":
             known_users.remove(name)
+        elif remove == 'n':
+            print("No problem. I didn't want you to leave anyway!")
 
     else:
-        print('Name not recognized')
+        print(f"Hmmm, I don't think I have met you yet {name}")
+        add_me = input("Would you like to be added to the system? (y/n) ".strip().lower())
+        if add_me == "y":
+            known_users.append(name)
+        elif add_me == "n":
+            print("No worries. See you around!")
+
+
+
 
 
