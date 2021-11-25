c0735a27e60f16c2aadde5898520d19d521f003a
pompyn <pompyn@live.com>
2021-11-25 15:01:57 -0600

    add list, greeting, user input name, known user check

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-
-
+known_users = ['Alice', 'Bob', 'Claire', 'Dan', 'Emma', 'Fred', 'Georgie', 'Harry']
+print(len(known_users))
+while True:
+    print('Hi, my name is Travis')
+    name = input('What is your name').strip().capitalize()
+    if name in known_users:
+        print('Name recognized')
+    else:
+        print('Name not recognized')
 
